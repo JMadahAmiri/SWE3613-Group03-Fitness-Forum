c2f95901952b4a2fbd14fa3a9e85860b3b095ee9
wmanintu <wutmanintu@icloud.com>
2014-11-23 15:31:51 -0500

    so the problem wasn't the code

--- index.php
+++ index.php
@@ -1,12 +1,55 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <!-- Set the viewport so this responsive site displays correctly on mobile devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Fitness Forum</title>
+    <!-- Include bootstrap CSS -->
+    <link href="includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="includes/style.css" rel="stylesheet">
 </head>
+    <!-- Site header and navigation -->
+    <header class="top" role="header">
+        <div class="container">
+            <a href="index.html" class="navbar-brand pull-left">
+				<!--<img class="imagebox" src="fitness-logo.jpeg">-->
+				Fitness Forum
+            </a>
+            <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="glyphicon glyphicon-align-justify"></span>
+            </button>
+            <nav class="navbar-collapse collapse" role="navigation">
+                <ul class="navbar-nav nav">
+                    <li><a href="login.php">Log In</a></li>
+					<li><a href="forums.php">Forum List</a></li>
+					<li><a href="recipes.php">Private Messages</a></li>
+					<li><a href="useraccount.php">User Account Panel</a></li>
+                    
+				</ul>
+            </nav>
+        </div>
+    </header>
 
 <body>
     <p>
-        You can see the index.php!
+        You can see me!
     </p>
+    <!-- Page Content -->
+	<div class="middle">
+		<div class="container">
+			<div class="well bs-component">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2><center>Bulletin Board</center></h2>
+						<p><center>Welcome to Fitness Forum. Join the community and talk about all things fitness. From hiking, to body-building, to swimming, we have a forum for you!</center></p>
+					</div>
+				</div>
+			</div>
+			<!-- /.well bs-component -->
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.middle -->
 </body>
 </html>
